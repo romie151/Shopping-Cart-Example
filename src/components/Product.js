514f45820910e2a73452ffcb45070c28465f9d2d
romie151 <romie151@yahoo.com>
2019-03-31 18:27:15 -0400

    headig style

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Product.css'
 
 const Product = ({ price, inventory, title }) => (
-  <div>
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
-  </div>
+  <div className="productContainer">
+    <div id="product">
+      <p>Title: {title}</p>
+      <p>Price: {price}</p>
+      <p>Inventory: {inventory}</p>
+      {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
+    </div>
+  </div >
 )
 
 Product.propTypes = {
